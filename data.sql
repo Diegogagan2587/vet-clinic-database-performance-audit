acc50e905e5f50cb9eefe8c536a7b3990603494e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-29 10:49:00 -0600

    Create new tables and add data

--- data.sql
+++ data.sql
@@ -1,5 +1,14 @@
-/* Populate database with sample data. */
+INSERT INTO animals (name) 
+VALUES ('Agumon'), 
+        ('Gabumon'), 
+        ('Pikachu'), 
+        ('Devimon'), 
+        ('Charmander'), 
+        ('Plantmon'), 
+        ('Squirtle'), 
+        ('Angemon'), 
+        ('Boarmon'), 
+        ('Blossom');
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO vets (name) 
+VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
--- schema.sql
+++ schema.sql
@@ -1,5 +1,47 @@
-/* Database schema to keep the structure of entire database. */
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(20),
+  age INT,
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
 );
